bd8ce8491d486d245fce87257281f8f64b1f5ed6
hadusha75-stack <hadusha75@gmail.com>
2026-02-16 22:03:01 +0300

    Attraction

--- lib/screens/search/Attractions.dart
+++ lib/screens/search/Attractions.dart
@@ -1,3 +1,5 @@
+import 'package:booking/controller/user_name_controller.dart';
+import 'package:booking/theam/app_color.dart';
 import 'package:booking/utils/location_popup.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
@@ -10,10 +12,14 @@ class Attractions extends StatefulWidget {
 }
 
 class _AttractionsState extends State<Attractions> {
-  final TextEditingController _dateController = Get.put(TextEditingController());
+  final TextEditingController _dateController = Get.put(
+    TextEditingController(),
+  );
+  final userNameController = Get.find<UserNameController>();
   @override
   Widget build(BuildContext context) {
     return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
       children: [
         Container(
           padding: const EdgeInsets.all(16),
@@ -35,7 +41,7 @@ class _AttractionsState extends State<Attractions> {
                         readOnly: true,
                         decoration: const InputDecoration(
                           border: InputBorder.none,
-                          hintText: 'Where do you wanna go',
+                          hintText: 'Where do you want to go',
                         ),
                         onTap: () {
                           showModalBottomSheet(
@@ -90,13 +96,15 @@ class _AttractionsState extends State<Attractions> {
                 child: Padding(
                   padding: const EdgeInsets.only(top: 10.0),
                   child: TextButton(
-                    onPressed: () {},
+                    onPressed: () {
+                      // Get.to();
+                    },
                     child: Text(
                       'Search',
                       style: TextStyle(color: Colors.white),
                     ),
                     style: TextButton.styleFrom(
-                      backgroundColor: Colors.blue,
+                      backgroundColor: AppColors.secondary,
                       shape: BeveledRectangleBorder(),
                     ),
                   ),
@@ -105,30 +113,308 @@ class _AttractionsState extends State<Attractions> {
             ],
           ),
         ),
+        Padding(
+          padding: const EdgeInsets.only(top: 20.0),
+          child: Text(
+            "Travel more, spend less",
+            style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+          ),
+        ),
+
         SingleChildScrollView(
           scrollDirection: Axis.horizontal,
           child: Padding(
-            padding: const EdgeInsets.only(top: 40.0),
-            child: Row(
-              children: [
-                Card(
-                  child: Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Text(
-                      'Genius\n XRESXSECCTTVYBIUU\n dtdt',
-                      style: TextStyle(color: Colors.white),
+            padding: const EdgeInsets.only(top: 20.0),
+            child:
+                // Obx(() =>
+                Row(
+                  children: [
+                    Card(
+                      child: Padding(
+                        padding: const EdgeInsets.all(28.0),
+                        child: SizedBox(
+                          width: 200,
+                          child: Text(
+                            'Genius\n'
+                            // '${userNameController.emailName.value}'
+                            'you are at Genius Level1 in our loyality program\n dtdt',
+                            style: TextStyle(
+                              color: AppColors.white,
+                              fontSize: 17,
+                            ),
+                          ),
+                        ),
+                      ),
+
+                      color: AppColors.secondary,
                     ),
-                  ),
-                  color: const Color.fromARGB(255, 2, 41, 237),
+                    SizedBox(width: 10),
+                    // Card(
+                    // child:
+                    Container(
+                      width: 200,
+                      height: 150,
+                      // margin: EdgeInsets.all(12),
+                      padding: EdgeInsets.all(12),
+                      decoration: BoxDecoration(
+                        color: AppColors.background,
+                        borderRadius: BorderRadius.circular(12),
+                        border: Border.all(
+                          color: AppColors.secondary,
+                          width: 1,
+                        ),
+                      ),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Row(
+                            children: [
+                              Expanded(
+                                child: Text(
+                                  '10% discount on stays\n',
+                                  style: TextStyle(fontWeight: FontWeight.bold),
+                                ),
+                              ),
+                              Padding(
+                                padding: const EdgeInsets.only(left: 15.0),
+                                child: Icon(
+                                  Icons.car_rental,
+                                  color: AppColors.blue,
+                                  size: 35,
+                                ),
+                              ),
+                            ],
+                          ),
+                          Text(
+                            'Enjoy discount at participating properties worldwide',
+                          ),
+                        ],
+                      ),
+                    ),
+
+                    SizedBox(width: 10),
+                    // Card(
+                    // child:
+                    Container(
+                      width: 200,
+                      height: 150,
+                      // margin: EdgeInsets.all(12),
+                      padding: EdgeInsets.all(12),
+                      decoration: BoxDecoration(
+                        color: AppColors.background,
+                        borderRadius: BorderRadius.circular(12),
+                        border: Border.all(
+                          color: AppColors.secondary,
+                          width: 1,
+                        ),
+                      ),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Row(
+                            children: [
+                              Expanded(
+                                child: Text(
+                                  '10% discount on rental cars\n',
+                                  style: TextStyle(fontWeight: FontWeight.bold),
+                                ),
+                              ),
+                              Padding(
+                                padding: const EdgeInsets.only(left: 15.0),
+                                child: Icon(
+                                  Icons.car_rental,
+                                  color: AppColors.blue,
+                                  size: 35,
+                                ),
+                              ),
+                            ],
+                          ),
+                          Text(
+                            'Enjoy discount at participating properties worldwide',
+                          ),
+                        ],
+                      ),
+                    ),
+                    SizedBox(width: 10),
+                    // Card(
+                    // child:
+                    Container(
+                      width: 200,
+                      height: 150,
+                      // margin: EdgeInsets.all(12),
+                      padding: EdgeInsets.all(12),
+                      decoration: BoxDecoration(
+                        color: AppColors.background,
+                        borderRadius: BorderRadius.circular(12),
+                        border: Border.all(
+                          color: AppColors.secondary,
+                          width: 1,
+                        ),
+                      ),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Row(
+                            children: [
+                              Expanded(
+                                child: Text(
+                                  '10% discount on stays\n',
+                                  style: TextStyle(fontWeight: FontWeight.bold),
+                                ),
+                              ),
+                              Padding(
+                                padding: const EdgeInsets.only(left: 15.0),
+                                child: Icon(
+                                  Icons.car_rental,
+                                  color: AppColors.blue,
+                                  size: 35,
+                                ),
+                              ),
+                            ],
+                          ),
+                          Text(
+                            'Enjoy discount at participating properties worldwide',
+                          ),
+                        ],
+                      ),
+                    ),
+                    SizedBox(width: 10),
+                    // Card(
+                    // child:
+                    Container(
+                      width: 200,
+                      height: 150,
+                      // margin: EdgeInsets.all(12),
+                      padding: EdgeInsets.all(12),
+                      decoration: BoxDecoration(
+                        color: AppColors.background,
+                        borderRadius: BorderRadius.circular(12),
+                        border: Border.all(
+                          color: AppColors.secondary,
+                          width: 1,
+                        ),
+                      ),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Row(
+                            children: [
+                              Expanded(
+                                child: Text(
+                                  '10% discount on stays\n',
+                                  style: TextStyle(fontWeight: FontWeight.bold),
+                                ),
+                              ),
+                              Padding(
+                                padding: const EdgeInsets.only(left: 15.0),
+                                child: Icon(
+                                  Icons.car_rental,
+                                  color: AppColors.blue,
+                                  size: 35,
+                                ),
+                              ),
+                            ],
+                          ),
+                          Text(
+                            'Enjoy discount at participating properties worldwide',
+                          ),
+                        ],
+                      ),
+                    ),
+                    SizedBox(width: 10),
+                    // Card(
+                    // child:
+                    Container(
+                      width: 200,
+                      height: 150,
+                      // margin: EdgeInsets.all(12),
+                      padding: EdgeInsets.all(12),
+                      decoration: BoxDecoration(
+                        color: AppColors.background,
+                        borderRadius: BorderRadius.circular(12),
+                        border: Border.all(
+                          color: AppColors.secondary,
+                          width: 1,
+                        ),
+                      ),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Row(
+                            children: [
+                              Expanded(
+                                child: Text(
+                                  '10% discount on stays\n',
+                                  style: TextStyle(fontWeight: FontWeight.bold),
+                                ),
+                              ),
+                              Padding(
+                                padding: const EdgeInsets.only(left: 15.0),
+                                child: Icon(
+                                  Icons.car_rental,
+                                  color: AppColors.blue,
+                                  size: 35,
+                                ),
+                              ),
+                            ],
+                          ),
+                          Text(
+                            'Enjoy discount at participating properties worldwide',
+                          ),
+                        ],
+                      ),
+                    ),
+                    SizedBox(width: 10),
+                    // Card(
+                    // child:
+                    Container(
+                      width: 200,
+                      height: 150,
+                      // margin: EdgeInsets.all(12),
+                      padding: EdgeInsets.all(12),
+                      decoration: BoxDecoration(
+                        color: AppColors.background,
+                        borderRadius: BorderRadius.circular(12),
+                        border: Border.all(
+                          color: AppColors.secondary,
+                          width: 1,
+                        ),
+                      ),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Row(
+                            children: [
+                              Expanded(
+                                child: Text(
+                                  '10% discount on stays\n',
+                                  style: TextStyle(fontWeight: FontWeight.bold),
+                                ),
+                              ),
+                              Padding(
+                                padding: const EdgeInsets.only(left: 15.0),
+                                child: Icon(
+                                  Icons.car_rental,
+                                  color: AppColors.blue,
+                                  size: 35,
+                                ),
+                              ),
+                            ],
+                          ),
+                          Text(
+                            'Enjoy discount at participating properties worldwide',
+                          ),
+                        ],
+                      ),
+                    ),
+                    // Card(child: Text('Genius\n XRESXSECCTTVYBIUU\n dtdt')),
+                    // Card(child: Text('Genius\n XRESXSECCTTVYBIUU\n dtdt')),
+                    // Card(child: Text('Genius\n XRESXSECCTTVYBIUU\n dtdt')),
+                  ],
                 ),
-                Card(child: Text('Genius\n XRESXSECCTTVYBIUU\n dtdt')),
-                Card(child: Text('Genius\n XRESXSECCTTVYBIUU\n dtdt')),
-                Card(child: Text('Genius\n XRESXSECCTTVYBIUU\n dtdt')),
-                Card(child: Text('Genius\n XRESXSECCTTVYBIUU\n dtdt')),
-              ],
-            ),
           ),
         ),
+        // ),
       ],
     );
   }
